b5db2a5758f04e15e9330e9d8919e6dbd63a0ffc
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2019-11-23 01:09:56 +0900

    Update to fit sentry v2

--- src/Twig/ContaoSentryTwigRuntime.php
+++ src/Twig/ContaoSentryTwigRuntime.php
@@ -4,42 +4,21 @@ declare(strict_types=1);
 
 namespace Oneup\Contao\SentryBundle\Twig;
 
-use Raven_Client;
+use Sentry\SentryBundle\SentryBundle;
 
 class ContaoSentryTwigRuntime
 {
-    /**
-     * @var Raven_Client
-     */
-    private $client;
-
-    /**
-     * AppRuntime constructor.
-     *
-     * @param Raven_Client $client
-     */
-    public function __construct(Raven_Client $client)
-    {
-        $this->client = $client;
-    }
-
-    /**
-     * Return the last captured event's ID or null if none available.
-     *
-     * @return string|null
-     */
     public function sentryLastEventIdFilter(): ?string
     {
-        return $this->client->getLastEventID();
+        return SentryBundle::getCurrentHub()->getLastEventId();
     }
 
-    /**
-     * Return the Sentry DSN. The DSN gets disassembled in the Raven_Client, so we build it here again.
-     *
-     * @return string
-     */
-    public function sentryDsn(): string
+    public function sentryDsn(): ?string
     {
-        return sprintf('https://'.$this->client->public_key.'@sentry.io/'.$this->client->project);
+        if (null !== $client = SentryBundle::getCurrentHub()->getClient()) {
+            return $client->getOptions()->getDsn();
+        }
+
+        return null;
     }
 }
